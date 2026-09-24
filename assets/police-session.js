67b9de4e82f613eea55c1998968f49724fd3d046
phantom-case-police <kaito.oji0608@gmail.com>
2026-09-24 21:07:24 +0900

    chat6

--- assets/police-session.js
+++
@@ -1,5 +0,0 @@
-
-(()=>{const el=document.querySelector("[data-session-return]");if(!el)return;
- const active=localStorage.getItem("phantom_case_chat_started")||localStorage.getItem("pc_chat_started")||sessionStorage.getItem("phantom_case_chat_started");
- if(active){el.classList.add("show");const a=el.querySelector("a");if(a)a.href="/phantom-case/police/chat/";}
-})();
